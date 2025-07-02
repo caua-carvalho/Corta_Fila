7e9995ff31342fe6f2ed9cc01d69284f633cf00b
caua-carvalho <caua.carma033@gmail.com>
2025-07-02 10:08:34 -0300

    endpoint: Validacao de token (evita que o usuario precise fazer login novamente)

--- back-end/public/validate.php
+++ back-end/public/validate.php
@@ -1,7 +1,11 @@
 <?php
-// public/validate.php
+// Ativa exibição de erros
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json; charset=utf-8');
-require __DIR__ . '/../src/auth.php';
+require __DIR__ . '/../Auth/auth.php';
 
 $userId = requireAuth();
 
